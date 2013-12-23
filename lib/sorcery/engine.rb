0fdba335f489a8b7f89231564314b2341850bf8a
Ernest Sim <ernest@anzu.sg>
2013-12-23 16:06:15 +0800

    Changed to support rails-api's ActionController::API controller

--- lib/sorcery/engine.rb
+++ lib/sorcery/engine.rb
@@ -6,16 +6,16 @@ module Sorcery
   # With the plugin logic.
   class Engine < Rails::Engine
     config.sorcery = ::Sorcery::Controller::Config
-    
+
     initializer "extend Controller with sorcery" do |app|
-      ActionController::Base.send(:include, Sorcery::Controller)
-      ActionController::Base.helper_method :current_user
-      ActionController::Base.helper_method :logged_in?
+      ActionController::API.send(:include, Sorcery::Controller)
+      ActionController::API.helper_method :current_user
+      ActionController::API.helper_method :logged_in?
     end
-    
+
     rake_tasks do
       load "sorcery/railties/tasks.rake"
     end
-    
+
   end
 end
